d024259944ca479d489b6286475c5a42a168f499
Jesus Marinez Ruano <jesus.martinez.ruano@gmail.com>
2019-03-24 19:39:24 +0100

    added pdf single repor with barcodes

--- react-redux-client/src/components/Proyector.js
+++ react-redux-client/src/components/Proyector.js
@@ -1,5 +1,39 @@
 // ./react-redux-client/src/components/Proyector.js
 import React from 'react';
+import  {Button,FormGroup,Table} from 'react-bootstrap'; //Added
+import jsPDF from 'jspdf';                                //Added
+
+import JsBarcode from 'jsbarcode';                      //Added
+
+
+
+var col1 = {                                                // added
+  textAlign : 'right',
+  fontWeight : 'bold',
+  borderBottom:'thin double #808080'
+};
+var col2 = {                                                //added
+  textAlign : 'left',
+  borderBottom:'thin double #808080'
+};
+
+function handleClick(proyector,e) {                         //added
+  
+  
+  e.preventDefault();
+  console.log('gen pdf was clicked.',proyector.ubiAlias);
+  console.log('Ubi alias: ', proyector.ubiAlias);
+  var canvas = document.createElement('CANVAS');
+  JsBarcode(canvas, proyector.ubiInvent);
+  var img = canvas.toDataURL();
+  var doc = new jsPDF('p','mm','a4');
+  doc.addImage(img,5,5,93.66,37.5);
+  JsBarcode(canvas, proyector.etiqueta);
+  var img = canvas.toDataURL();
+  doc.addImage(img,100,5,52.92,37.5);
+  doc.save("test.pdf");
+}
+
 
 export default class Proyector extends React.Component {
   componentDidMount(){
@@ -8,34 +42,51 @@ export default class Proyector extends React.Component {
 
   render(){
     const proyectorState = this.props.mappedProyectorState;
+    //console.log("id : ",this.props.params.id);
+    if (!proyectorState.isFetching) {
+      //console.log("ubi alias : ", proyectorState.ubiAlias);
+    }
     
-
     return(
-      <div className="proyectorDetail">
-       <h2>Proyector Detail</h2>
+      
+      
+      <div>
+      
+      <h2>Detalles del Proyector</h2>
          {!proyectorState.proyector && proyectorState.isFetching &&
            <div>
-             <p>Loading proyector....</p>
+             <p>Cargando proyector....</p>
            </div>
          }
        {proyectorState.proyector && !proyectorState.isFetching &&
-         <div>
-           <h3>{proyectorState.proyector.ubiAlias}</h3>
-           <p>{proyectorState.proyector.ubiInvent}</p>
-           <p>{proyectorState.proyector.marca}</p>
-           <p>{proyectorState.proyector.modelo}</p>
-           <p>{proyectorState.proyector.etiqueta}</p>
-           <p>{proyectorState.proyector.etiquetado}</p>
-           <p>{proyectorState.proyector.año}</p>
-           <p>{proyectorState.proyector.sn}</p>
-           <p>{proyectorState.proyector.ip}</p>
-           <p>{proyectorState.proyector.password}</p>
-           <p>{proyectorState.proyector.operable}</p>
-           <p>{proyectorState.proyector.observaciones}</p>
-
-         </div>
+        <div>                 
+          <Table>
+            <tbody>
+              <tr><td style={col1}></td><td style={col2}><h3>{proyectorState.proyector.ubiAlias}</h3></td></tr>
+              <tr><td style={col1}>Ubicación de inventario &nbsp; </td><td style={col2}>{proyectorState.proyector.ubiInvent}</td></tr>
+              <tr><td style={col1}>Marca &nbsp; </td><td style={col2}>{proyectorState.proyector.marca}</td></tr>
+              <tr><td style={col1}>Modelo &nbsp; </td><td style={col2}>{proyectorState.proyector.modelo}</td></tr>
+              <tr><td style={col1}>Etiqueta &nbsp; </td><td style={col2}>{proyectorState.proyector.etiqueta}</td></tr>
+              <tr><td style={col1}>Etiquetado &nbsp; </td><td style={col2}>{proyectorState.proyector.etiquetado}</td></tr>
+              <tr><td style={col1}>Año &nbsp; </td><td style={col2}>{proyectorState.proyector.año}</td></tr>
+              <tr><td style={col1}>Número de serie &nbsp; </td><td style={col2}>{proyectorState.proyector.sn}</td></tr>
+              <tr><td style={col1}>IP &nbsp; </td><td style={col2}>{proyectorState.proyector.ip}</td></tr>
+              <tr><td style={col1}>Password &nbsp; </td><td style={col2}>{proyectorState.proyector.password}</td></tr>
+              <tr><td style={col1}>Operable &nbsp; </td><td style={col2}>{proyectorState.proyector.operable}</td></tr>
+              <tr><td style={col1}>Observaciones &nbsp; </td><td style={col2}>{proyectorState.proyector.observaciones}</td></tr>              
+            </tbody>
+          </Table>
+          <form>
+            <FormGroup>
+              <Button type="submit" bsStyle="danger" bsSize="small" block onClick={(e)=>handleClick(proyectorState.proyector,e)}>Generar PDF</Button>
+              
+            </FormGroup>
+          </form>          
+        </div>
+        
        }
       </div>
+    
     );
   }
 }
